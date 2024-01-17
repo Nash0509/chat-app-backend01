a486c345711d5db9ddaa3c9de6a5686dd5ea7640
unknown <nishantsinghworkshard@gmail.com>
2024-01-17 19:33:14 +0530

    All can access

--- backend.js
+++ backend.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app)
 const io = new Server(server, {
     cors : {
-        origin: "https://chat-app-roan-nu.vercel.app/",
+        origin: "*",
         methods: ["GET", "POST"],
     }
 })
